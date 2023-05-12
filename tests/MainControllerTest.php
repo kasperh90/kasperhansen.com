c116dea5edd66e0ce4f37e04159802bec4288b58
Kasper Hansen <kasper.h90@outlook.dk>
2023-05-12 20:38:27 +0200

    Created test files for main controllers

---
+++ tests/MainControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MainControllerTest extends WebTestCase
+{
+
+    public function testIfClassExists () : void {
+
+        $this->fail();
+
+    }
+
+}
