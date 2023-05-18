5a9a9c8012d04a00f26c88cd414c55837925905c
Kasper Hansen <kasper.h90@outlook.dk>
2023-05-18 15:38:44 +0200

    Added user and test for it

--- tests/Unit/Entity/UserTest.php
+++ tests/Unit/Entity/UserTest.php
@@ -4,14 +4,37 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Entity;
 
+use App\Entity\User;
+use Generator;
 use Monolog\Test\TestCase;
 
 class UserTest extends TestCase
 {
 
-    public function testItWorks(): void
+
+    /**
+     * @dataProvider userDataProvider
+     */
+    public function testUserGettersAndSetters(
+        string $email,
+        string $password,
+        array  $roles,
+        int    $numberOfRoles): void
     {
-        $this->fail();
+        $user = new User();
+        $user->setEmail($email);
+        $user->setPassword($password);
+        $user->setRoles($roles);
+
+        $this->assertSame($email, $user->getEmail());
+        $this->assertSame($password, $user->getPassword());
+        $this->assertTrue(count($user->getRoles()) === $numberOfRoles);
+    }
+
+    public function userDataProvider(): Generator
+    {
+        yield ['test@kasperhansen.com', 'eiufghy38tf87w7', [], 1];
+        yield ['*#/)(/=', '&)/&(&(', ['ROLE_ADMIN'], 2];
     }
 
 }
